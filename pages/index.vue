d8f000d342cba9b49b800b09382cbddd27c520ab
Aleks <san4es.kom@gmail.com>
2024-03-06 12:15:26 +0200

    fix: Initial path fixed

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script>
+import Header from '../components/Header.vue'
+import Main from '../components/Main.vue'
+export default {
+  components: {
+    Header,
+    Main,
+  },
+
+}
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style scoped>
+
+</style>
