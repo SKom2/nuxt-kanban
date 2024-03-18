35fb3de39dc1240ee16df9e023392e44701acb22
Aleks <san4es.kom@gmail.com>
2024-03-18 10:53:27 +0200

    Updates

--- dist/server/_nuxt/Tasks-styles-1.mjs-ZXLi3dLp.js
+++
@@ -1,5 +0,0 @@
-const Tasks_vue_vue_type_style_index_0_scoped_d779935a_lang = ".ghost-class[data-v-d779935a]{background:#474747;border-radius:6px}.ghost-class>div[data-v-d779935a]{visibility:hidden}";
-export {
-  Tasks_vue_vue_type_style_index_0_scoped_d779935a_lang as default
-};
-//# sourceMappingURL=Tasks-styles-1.mjs-ZXLi3dLp.js.map
--- dist/server/_nuxt/Tasks-styles.DpKmCBkX.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './Tasks-styles-1.mjs-ZXLi3dLp.js';
-export default [style_0]
